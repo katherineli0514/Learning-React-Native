0a7f819cb6791931e5de4c9f9316e628b2e40632
Katherine Li <katherineli0514@gmail.com>
2019-04-19 15:53:14 -0700

    Initial project.

---
+++ AwesomeProject/index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+//import App from './source/App';
+//import App from './source/Banana';
+//import App from './source/LotsOfGreetings';
+import App from './source/BlinkApp';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
